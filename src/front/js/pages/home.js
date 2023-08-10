e6936fb5d62dca43fed3a0a2893d631b2280f679
Edgar Diaz <ed35376@gmail.com>
2023-08-10 17:02:34 -0400

    home boton segun seleccion

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,49 +1,63 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
-
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
+  const [selectedService, setSelectedService] = useState(""); // Estado para el tipo de servicio seleccionado
+
+  const handleServiceChange = (event) => {
+    setSelectedService(event.target.value); // Actualiza el estado con el tipo de servicio seleccionado
+  };
+
   return (
     <div className="text-center mt-5">
       <h1>Solicitud</h1>
       <div className="container">
         Tipo de servicio
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Open this select menu</option>
-          <option value="1">Colocacion en Rack</option>
-          <option value="2">Colocacion Catalogado</option>
+        <select
+          className="form-select"
+          aria-label="Default select example"
+          onChange={handleServiceChange}
+        >
+          <option value="">Seleccionar servicio</option>
+          <option value="rack">Colocacion en Rack</option>
+          <option value="equipment">Colocacion Catalogado</option>
         </select>
       </div>
 
-	  <div className="container">
-        Cliente
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Open this select menu</option>
-          <option value="1">Lista desde bd</option>
-          <option value="2">Otro</option>
-        </select>
-      </div>
-	  <div className="m-auto col-4">
-            <label for="finalClient" className="form-label">
-              Nombre del CLiente Final
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="finalClient"
-              placeholder="Ingrese el nombre del cliente final"
-            />
-          </div>
-      <Link to="/rack">
-        <button>Agregar Rack</button>
-      </Link>
-      <Link to="/equipment">
-        <button>Agregar Equipo</button>
-      </Link>
+      {selectedService === "rack" && (
+        <Link to="/rack">
+          <button>Agregar Rack</button>
+        </Link>
+      )}
+
+      {selectedService === "equipment" && (
+        <>
+          <Link to="/rack">
+            <button>Agregar Rack</button>
+          </Link>
+          <Link to="/equipment">
+            <button>Agregar Equipo</button>
+          </Link>
+        </>
+      )}
+
+      {selectedService && (
+        <div className="m-auto col-4">
+          <label htmlFor="finalClient" className="form-label">
+            Nombre del Cliente Final
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="finalClient"
+            placeholder="Ingrese el nombre del cliente final"
+          />
+        </div>
+      )}
 
       <p className="alert alert-info">Tu solicitud</p>
 
