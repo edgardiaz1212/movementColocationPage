8a3654f22a2703906e55c1269c7ff5d5eabfc2f4
Edgar Diaz <ed35376@gmail.com>
2023-08-14 20:59:19 -0400

    name on rack inputs

--- src/front/js/pages/Rack.jsx
+++ src/front/js/pages/Rack.jsx
@@ -5,30 +5,52 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 function Rack() {
-  const [hasCabinet, setHasCabinet] = useState(true); 
-  const [hasExtractors, setHasExtractors] = useState(false);
-  const [hasLateralDoors, setHasLateralDoors] = useState(false);
-  const [hasAccesories, setHasAccesories] = useState(false);
+  // const [hasCabinet, setHasCabinet] = useState(true); 
+  // const [hasExtractors, setHasExtractors] = useState(false);
+  // const [hasLateralDoors, setHasLateralDoors] = useState(false);
+  // const [hasAccesories, setHasAccesories] = useState(false);
   const { actions } = useContext(Context)
 
-  const handleHasCabinetChange = (event) => {
-    setHasCabinet(event.target.id === "siGabinete");
+  // const handleHasCabinetChange = (event) => {
+  //   setHasCabinet(event.target.id === "siGabinete");
+  // };
+
+  // const handleHasExtractorsChange = (event) => {
+  //   setHasExtractors(event.target.id === "yesExtractor");
+  // };
+
+  // const handleHasLateralDoorsChange = (event) => {
+  //   setHasLateralDoors(event.target.id === "yesLateralDoors");
+  // };
+
+  // const handleHasAccesoriesChange = (event) => {
+  //   setHasAccesories(event.target.id === "yesAccessories");
+  // };
+  const [formData, setFormData] = useState({
+    hasCabinet: true,
+    leased:true,
+    hasExtractors: false,
+    hasLateralDoors: false,
+    hasAccesories: false,
+    totalGabinetes: "",
+    tipoSeguridad: "",
+    // ...otros campos del formulario
+  });
+
+  const handleFieldChange = (event) => {
+    const { id, value, type, checked } = event.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: type === "checkbox" ? checked : value,
+    }));
   };
 
-  const handleHasExtractorsChange = (event) => {
-    setHasExtractors(event.target.id === "yesExtractor");
-  };
-
-  const handleHasLateralDoorsChange = (event) => {
-    setHasLateralDoors(event.target.id === "yesLateralDoors");
-  };
-
-  const handleHasAccesoriesChange = (event) => {
-    setHasAccesories(event.target.id === "yesAccessories");
-  };
-
-const HandleAddRack = () => {
+const handleAddRack = () => {
   const formData = new FormData(document.getElementById("rackForm"));
+  formData.append("client", actions.store.clientName);
+  formData.append("contract", actions.store.selectedContract);
+  formData.append("service", actions.store.selectedService);
   actions.addNewRack(formData);
 };
 
@@ -48,10 +70,10 @@ const HandleAddRack = () => {
               <input
                 className="form-check-input"
                 type="radio"
-                name="flexRadioOwn"
+                name="hasCabinet"
                 id="siGabinete"
                 value="siGabinete"
-                onChange={handleHasCabinetChange}
+                onChange={handleFieldChange}
               />
               <label className="form-check-label" htmlFor="siGabinete">
                 Si
@@ -61,18 +83,18 @@ const HandleAddRack = () => {
               <input
                 className="div-check-input"
                 type="radio"
-                name="flexRadioOwn"
+                name="hasCabinet"
                 id="noGabinete"
                 value="noGabinete"
-                checked={!hasCabinet}
-                onChange={handleHasCabinetChange}
+                checked={!formData.hasCabinet}
+                onChange={handleFieldChange}
               />
               <label className="form-check-label" htmlFor="noGabinete">
                 No
               </label>
             </div>
           </div>
-          {hasCabinet && (
+          {formData.hasCabinet && (
             <>
               <div className="col-4 m-auto">
                 <p>Propio o arrendado ?</p>
@@ -80,7 +102,7 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioLease"
+                    name="leased"
                     id="noLeased"
                   />
                   <label className="form-check-label" htmlFor="noLeased">
@@ -91,7 +113,7 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioLease"
+                    name="leased"
                     id="leased"
                     checked
                   />
@@ -108,6 +130,7 @@ const HandleAddRack = () => {
                   type="text"
                   className="form-control"
                   id="totalGabinetes"
+                  name="total_cabinets"
                   placeholder="Total de gabinetes que se requiere instalar"
                 />
               </div>
@@ -118,6 +141,7 @@ const HandleAddRack = () => {
                 <input
                   type="text"
                   className="form-control"
+                  name="security"
                   id="tipoSeguridad"
                   placeholder="tipo de seguridad y cantidad"
                 />
@@ -128,9 +152,9 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioExtractor"
+                    name="has_extractors"
                     id="yesExtractor"
-                    onChange={handleHasExtractorsChange}
+                    onChange={handleFieldChange}
                   />
                   <label className="form-check-label" htmlFor="yesExtractor">
                     Si
@@ -140,17 +164,17 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioExtractor"
+                    name="has_extractors"
                     id="noExtractor"
-                    checked={!hasExtractors}
-                    onChange={handleHasExtractorsChange}
+                    checked={!formData.hasExtractors}
+                    onChange={handleFieldChange}
                   />
                   <label className="form-check-label" htmlFor="noExtractor">
                     No
                   </label>
                 </div>
               </div>
-              {hasExtractors && (
+              {formData.hasExtractors && (
                 <div className="m-auto col-4">
                   <label htmlFor="extractores" className="form-label">
                     Si la respuesta es SI, Dónde:
@@ -158,6 +182,7 @@ const HandleAddRack = () => {
                   <input
                     type="text"
                     className="div-control"
+                    name="extractors_ubication"
                     id="extractores"
                     placeholder="Introduzca la ubicacion de los extractores"
                   />
@@ -169,7 +194,7 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioModular"
+                    name="modular"
                     id="yesModular"
                   />
                   <label className="form-check-label" htmlFor="yesModular">
@@ -180,7 +205,7 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioModular"
+                    name="modular"
                     id="noModular"
                     checked
                   />
@@ -195,9 +220,9 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioDoors"
+                    name="lateral_doors"
                     id="yesLateralDoors"
-                    onChange={handleHasLateralDoorsChange}
+                    onChange={handleFieldChange}
                   />
                   <label className="form-check-label" htmlFor="yesLateralDoors">
                     Si
@@ -207,17 +232,17 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioDoors"
+                    name="lateral_doors"
                     id="noLateralDoors"
-                    checked={!hasLateralDoors}
-                    onChange={handleHasLateralDoorsChange}
+                    checked={!formData.hasLateralDoors}
+                    onChange={handleFieldChange}
                   />
                   <label className="form-check-label" htmlFor="noLateralDoors">
                     No
                   </label>
                 </div>
               </div>
-              {hasLateralDoors && (
+              {formData.hasLateralDoors && (
                 <div className="m-auto col-4">
                   <label htmlFor="lateralDoors" className="form-label">
                     Si la respuesta es SI, Dónde:
@@ -225,6 +250,7 @@ const HandleAddRack = () => {
                   <input
                     type="text"
                     className="form-control"
+                    name='lateral_ubication'
                     id="lateralDoors"
                     placeholder="Introduzca la ubicacion de las Puertas de Servicio"
                   />
@@ -237,6 +263,7 @@ const HandleAddRack = () => {
                 <input
                   type="text"
                   className="form-control"
+                  name="rack_unit"
                   id="RURack"
                   placeholder="Introduzca la cantidad total de Unidades de Rack"
                 />
@@ -249,6 +276,7 @@ const HandleAddRack = () => {
                   type="text"
                   className="form-control"
                   id="rackPosition"
+                  name="rack_position"
                   placeholder="Introduzca la posicion del rack respecto a los demas"
                 />
               </div>
@@ -258,9 +286,9 @@ const HandleAddRack = () => {
                   <input
                     className="form-check-input"
                     type="radio"
-                    name="flexRadioAccessories"
+                    name="has_accessory"
                     id="yesAccessories"
-                    onChange={handleHasAccesoriesChange}
+                    onChange={handleFieldChange}
                   />
                   <label className="form-check-label" htmlFor="yesAccessories">
                     Sí
@@ -270,10 +298,10 @@ const HandleAddRack = () => {
                   <input
                     className="div-check-input"
                     type="radio"
-                    name="flexRadioAccessories"
+                    name="has_accessory"
                     id="noAccessories"
-                    checked={!hasAccesories}
-                    onChange={handleHasAccesoriesChange}
+                    checked={!formData.hasAccesories}
+                    onChange={handleFieldChange}
                   />
                   <label className="form-check-label" htmlFor="noAccessories">
                     No
@@ -281,7 +309,7 @@ const HandleAddRack = () => {
                 </div>
               </div>
 
-              {hasAccesories && (
+              {formData.hasAccesories && (
                 <div className="m-auto col-4">
                   <label htmlFor="accessoryDescription" className="form-label">
                     Describa los accesorios adicionales:
@@ -289,6 +317,7 @@ const HandleAddRack = () => {
                   <input
                     type="text"
                     className="form-control"
+                    name="accessory_description"
                     id="accessoryDescription"
                     placeholder="Descripción de los accesorios adicionales"
                   />
@@ -304,6 +333,7 @@ const HandleAddRack = () => {
                     <input
                       type="text"
                       className="form-control"
+                      name="rack_width"
                       id="rackWidth"
                       placeholder="Introduzca el ancho del rack"
                     />
@@ -316,6 +346,7 @@ const HandleAddRack = () => {
                     <input
                       type="text"
                       className="form-control"
+                      name="rack_length"
                       id="rackLength"
                       placeholder="Introduzca el largo del rack"
                     />
@@ -328,6 +359,7 @@ const HandleAddRack = () => {
                     <input
                       type="text"
                       className="form-control"
+                      name="rack_height"
                       id="rackHeight"
                       placeholder="Introduzca el alto del rack"
                     />
@@ -346,7 +378,20 @@ const HandleAddRack = () => {
             type="text"
             className="form-control"
             id="internalPdu"
+            name="internal_pdu"
             placeholder="Introduzca la cantidad de PDUs internas"
+          />
+        </div>
+        <div className="mb-3 col-4">
+          <label htmlFor="input_connector" className="form-label">
+            Unidades de Distribución de Energía (PDU) Internas:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="input_connector"
+            name="input_connector"
+            placeholder="Introduzca tipo de conector"
           />
         </div>
 
@@ -357,19 +402,21 @@ const HandleAddRack = () => {
           <input
             type="text"
             className="form-control"
+            name="fases"
             id="phasesPdu"
             placeholder="Introduzca la cantidad de Fases para la PDU"
           />
         </div>
 
         <div className="mb-3 col-4">
-          <label htmlFor="inputPdu" className="form-label">
+          <label htmlFor="output_connector" className="form-label">
             Numero de Tomas
           </label>
           <input
             type="text"
             className="form-control"
-            id="inputPdu"
+            id="output_connector"
+            name="output_connector"
             placeholder="Introduzca la cantidad de Tomas de la PDU"
           />
         </div>
@@ -379,7 +426,7 @@ const HandleAddRack = () => {
           <input
             className="form-check-input"
             type="radio"
-            name="flexRadioNeutro"
+            name="neutro"
             id="yesNeutro"
           />
           <label className="form-check-label" htmlFor="yesNeutro">
@@ -390,7 +437,7 @@ const HandleAddRack = () => {
           <input
             className="form-check-input"
             type="radio"
-            name="flexRadioNeutro"
+            name="neutro"
             id="noNeutro"
             checked
           />
@@ -403,7 +450,7 @@ const HandleAddRack = () => {
       </div>
       <Link to="/">
       <button
-      onClick={HandleAddRack}
+      onClick={handleAddRack}
       type="button">Agregar</button>
       </Link>
       </form>
