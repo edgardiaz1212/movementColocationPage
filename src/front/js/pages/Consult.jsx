f5b0ba6f5bbe6cd6aaf8b7c6690e1f4f83680024
Edgar Diaz <ed35376@gmail.com>
2023-09-04 21:59:28 -0400

    boton delete creado

--- src/front/js/pages/Consult.jsx
+++ src/front/js/pages/Consult.jsx
@@ -1,12 +1,10 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-//un useeffect para que se traiga aca los equipos guardados en la base de datos
 
 function Consult() {
     const { actions, store } = useContext(Context);
     useEffect(() => {
-        // Llamar a las funciones para obtener los datos de los racks y equipos aquí
         actions.getRackByUser();
         actions.getEquipmentByUser();
         actions.getUserData();
@@ -16,93 +14,139 @@ function Consult() {
     const hasRackData = store.racksData.length > 0;
     const hasEquipmentData = store.equipmentsData.length > 0;
 
-    // Función para obtener los equipos asociados a un rack específico
     const getEquipmentsForRack = (rackNumber) => {
         return store.equipmentsData.filter((equipment) => equipment.rack_number === rackNumber);
     };
-    console.log(store.equipmentsData)
+
+    // Filtra los equipos que no están asignados a ningún rack
+    const equipmentsWithoutRack = store.equipmentsData.filter(
+        (equipment) => !equipment.rack_number || !store.racksData.some((rack) => rack.rack_position === equipment.rack_number)
+    );
+
     return (
-        <>
+        <div className="container">
+            <h4>Planillas de Solicitud para {store.userData.clientName} en el Contexto del Servicio {store.userData.service} y el Contrato {store.userData.contract}</h4>
             <div className="container">
-                <h4>Planillas de Solicitud para {store.userData.clientName} en el Contexto del Servicio {store.userData.service} y el Contrato {store.userData.contract}</h4>
-                <div className="container ">
-                    <div className="row border border-black">
-                        {isNotCatalog && (
-                            <>
-                                {hasRackData ? (
-                                    <>
-                                        <div className="col-12 border border-danger">
-                                            {/* aca un condicion que si es contrato calogado solo muestre el map de los equiops.
-                                    otra condicion que si es no catalogado y solo se agrego equipo mostrar un bloque de rack pero con mensaje debe anadir el rack numero x */}
-                                            {store.racksData.map((rack) => (
-                                                <div key={rack.id}>
-                                                    <h2>RACK número {rack.rack_position}</h2>
-                                                    <table className="table table-striped">
-                                                        <thead>
-                                                            <tr>
-                                                                <th>Equipos en Rack Posicion {rack.rack_position}</th>
-                                                                {/* Agrega otras columnas de racks aquí */}
-                                                            </tr>
-                                                            <Link to="/rack" className="btn btn-primary m-1">Editar</Link>
-                                                            <Link to="" className="btn btn-primary m-1">Ver Planilla</Link>
-                                                        </thead>
-                                                        <tbody>
-                                                            {getEquipmentsForRack(rack.rack_position).map((equipment) => (
-                                                                <>
-                                                                    <tr key={equipment.id}>
+                <div className="row border border-secondary">
+                    {isNotCatalog && hasRackData && (
+                        <div className="col-12 ">
+                            {store.racksData.some((rack) => getEquipmentsForRack(rack.rack_position).length === 0) ? (
+                                <div className="alert alert-warning" role="alert">
+                                    ¡Advertencia! Hay racks sin equipos asociados.
+                                </div>
+                            ) : null}
+                            {store.racksData.map((rack) => (
+                                <div key={rack.id}>
+                                    <div className="border-top border-bottom ">
+                                        <h2 >RACK {rack.rack_ubication} posicion {rack.rack_position} </h2>
+                                        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                            <Link to="/rack" className="btn btn-primary btn-sm m-1">Editar</Link>
+                                            <Link to="" className="btn btn-primary btn-sm m-1">Ver Planilla</Link>
+                                            <button className="btn btn-primary btn-sm m-1"> Eliminar</button>
+                                        </div>
+                                    </div>
+                                    <table className="table table-striped">
+                                        <thead className="border-top border-bottom ">
+                                            <tr>
+                                                <th>Equipos en Rack {rack.rack_ubication} Posicion {rack.rack_position}</th>
 
-                                                                        <td>{equipment.rack_number}, el componente  {equipment.description.componentType}</td>
-                                                                        {/* Agrega otras celdas de datos del equipo aquí */}
+                                            </tr>
 
-                                                                    </tr>
-                                                                    <Link to="/equipment" className="btn btn-primary m-1">Editar</Link>
-                                                                    <Link to="" className="btn btn-primary m-1">Ver Planilla</Link>
-                                                                </>
-                                                            ))}
-                                                        </tbody>
-                                                    </table>
-                                                </div>
+                                        </thead>
+                                        <tbody>
+                                            {getEquipmentsForRack(rack.rack_position).map((equipment) => (
+                                                <>
+                                                    <tr key={equipment.id}>
+                                                        <td>El componente {equipment.description.componentType} modelo {equipment.description.model}</td>
+
+                                                        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                                            <Link to="/equipment" className="btn btn-primary btn-sm m-1">Editar</Link>
+                                                            <Link to="" className="btn btn-primary btn-sm m-1">Ver Planilla</Link>
+                                                            <button className="btn btn-primary btn-sm m-1"> Eliminar</button>
+                                                        </div>
+
+                                                    </tr>
+
+                                                </>
                                             ))}
-                                        </div>
-                                    </>
-                                ) : (
-                                    <>
-                                        <div className="alert alert-warning" role="alert">
-                                            Agrega el rack donde sera instalado el equipo
-                                        </div>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    {isNotCatalog && hasEquipmentData && (
+                        <div className="col-12 p-2 border-top border-secondary">
+                            {equipmentsWithoutRack.length > 0 && (
+                                <div className="alert alert-warning" role="alert">
+                                    Estos equipos no están asociados a ningún rack o no hay un rack con su posición correspondiente
+                                    {/* {equipmentsWithoutRack
+                                        .map((equipment) => equipment.rack_number)
+                                        .join(", ")} */}
+                                </div>
+                            )}
+                            <table className="table table-striped">
+                                <thead className="border-top border-bottom ">
+                                    <tr>
+                                        <th>Equipo</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {equipmentsWithoutRack.map((equipment) => (
 
-                                        <table className="table table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th>Equipo</th>
-                                                    {/* Agrega otras columnas de racks aquí */}
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {store.equipmentsData.map((equipment) => (
-                                                    <>
-                                                        <tr key={equipment.id}>
-                                                            <td>{equipment.rack_number}</td>
-                                                            
-                                                        </tr>
-                                                    </>
-                                                ))}</tbody>
-                                        </table>
-                                    </>
-                                )}
-                            </>
-                        )}
+                                        <tr key={equipment.id}>
+                                            <td>Componente tipo {equipment.description.componentType}, modelo {equipment.description.model} para asociar con rack {equipment.rack_number}</td>
+                                            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                                <Link to="/equipment" className="btn btn-primary btn-sm m-1">Editar</Link>
+                                                <Link to="" className="btn btn-primary btn-sm m-1">Ver Planilla</Link>
+                                                <button className="btn btn-primary btn-sm m-1"> Eliminar</button>
+                                            </div>
+                                        </tr>
 
-                    </div>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
                 </div>
-                <p>Seleccionar otra actividad</p>
-                {isNotCatalog && (
-                    <Link to="/rack" className="btn btn-primary m-1">Agregar nuevo Rack</Link>
+
+                {!isNotCatalog && (
+                    <div className="col-12">
+
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Equipamiento registrado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {store.equipmentsData.map((equipment) => (
+                                    <>
+                                        <tr key={equipment.id}>
+                                            <td>Componentes {equipment.description.componentType}</td>
+                                        </tr>
+                                        <Link to="/equipment" className="btn btn-primary btn-sm m-1">Editar</Link>
+                                        <Link to="" className="btn btn-primary btn-sm m-1">Ver Planilla</Link>
+                                        <button className="btn btn-primary btn-sm m-1"> Eliminar</button>
+                                    </>))}
+                            </tbody>
+                        </table>
+                    </div>
+
                 )}
-                <Link to="/equipment" className="btn btn-primary">Agregar nuevo Equipo</Link>
-                <button>Finalizar</button>
             </div>
-        </>
+
+            <p>Seleccionar otra actividad</p>
+            {isNotCatalog && (
+                <Link to="/rack" className="btn btn-primary m-1">
+                    Agregar nuevo Rack
+                </Link>
+            )}
+            <Link to="/equipment" className="btn btn-primary">
+                Agregar nuevo Equipo
+            </Link>
+            <button>Finalizar</button>
+        </div>
     );
 }
 
