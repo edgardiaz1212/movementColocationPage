e1640d3cd997ca4471892fc7f1dfefc9bf5cef67
Edgar Diaz <127860772+edgardiaz1212@users.noreply.github.com>
2023-09-05 18:37:17 +0000

    pdfvierw

---
+++ src/front/js/pages/PDFView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//aca hay que importar e instalar react pdf . para visualizar pdf existe pdf viewer
+
+function PDFView() {
+    return (
+        <div>PDF</div>
+
+    )
+
+}
+export default PDFView
