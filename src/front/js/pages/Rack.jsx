9bf0a3b4c7a669fe70b681ecb0f951535bd87b04
Edgar Diaz <ed35376@gmail.com>
2023-09-27 17:34:32 -0400

    boton agregar

--- src/front/js/pages/Rack.jsx
+++ src/front/js/pages/Rack.jsx
@@ -68,64 +68,64 @@ function Rack() {
   };
 
   const handleAddRack = async () => {
-    if (!data.brand || !data.model || !data.serial || !data.componentType ) {
+    if (!data.brand || !data.model || !data.serial || !data.componentType) {
       console.log("faltan datos importantes")
       toast.error("Llene los campos necesarios")
       return
-    } 
-      try {
-        const formData = new FormData();
+    }
+    try {
+      const formData = new FormData();
 
-        formData.append("model", data.model)
-        formData.append("brand", data.brand)
-        formData.append("serial", data.serial)
-        formData.append("number_part", data.number_part)
-        formData.append("componentType", data.componentType)
-        formData.append("five_years_prevition", data.five_years_prevition)
-        formData.append("observations", data.observations)
-        formData.append("has_cabinet", data.has_cabinet)
-        formData.append('leased', data.leased)
-        formData.append('total_cabinets', data.total_cabinets)
-        formData.append('open_closed', data.open_closed)
-        formData.append('security', data.security)
-        formData.append('type_security', data.type_security)
-        formData.append('has_extractors', data.has_extractors)
-        formData.append('extractors_ubication', data.extractors_ubication)
-        formData.append('modular', data.modular)
-        formData.append('lateral_doors', data.lateral_doors)
-        formData.append('lateral_ubication', data.lateral_ubication)
-        formData.append('rack_unit', data.rack_unit)
-        formData.append('rack_position', data.rack_position)
-        formData.append('rack_ubication', data.rack_ubication)
-        formData.append('has_accessory', data.has_accessory)
-        formData.append('accessory_description', data.accessory_description)
-        formData.append('rack_width', data.rack_width)
-        formData.append('rack_length', data.rack_length)
-        formData.append('rack_height', data.rack_height)
-        formData.append('internal_pdu', data.internal_pdu)
-        formData.append('input_connector', data.input_connector)
-        formData.append('fases', data.fases)
-        formData.append('output_connector', data.output_connector)
-        formData.append('neutro', data.neutro)
-        formData.append('user_id', currentUserId)
+      formData.append("model", data.model)
+      formData.append("brand", data.brand)
+      formData.append("serial", data.serial)
+      formData.append("number_part", data.number_part)
+      formData.append("componentType", data.componentType)
+      formData.append("five_years_prevition", data.five_years_prevition)
+      formData.append("observations", data.observations)
+      formData.append("has_cabinet", data.has_cabinet)
+      formData.append('leased', data.leased)
+      formData.append('total_cabinets', data.total_cabinets)
+      formData.append('open_closed', data.open_closed)
+      formData.append('security', data.security)
+      formData.append('type_security', data.type_security)
+      formData.append('has_extractors', data.has_extractors)
+      formData.append('extractors_ubication', data.extractors_ubication)
+      formData.append('modular', data.modular)
+      formData.append('lateral_doors', data.lateral_doors)
+      formData.append('lateral_ubication', data.lateral_ubication)
+      formData.append('rack_unit', data.rack_unit)
+      formData.append('rack_position', data.rack_position)
+      formData.append('rack_ubication', data.rack_ubication)
+      formData.append('has_accessory', data.has_accessory)
+      formData.append('accessory_description', data.accessory_description)
+      formData.append('rack_width', data.rack_width)
+      formData.append('rack_length', data.rack_length)
+      formData.append('rack_height', data.rack_height)
+      formData.append('internal_pdu', data.internal_pdu)
+      formData.append('input_connector', data.input_connector)
+      formData.append('fases', data.fases)
+      formData.append('output_connector', data.output_connector)
+      formData.append('neutro', data.neutro)
+      formData.append('user_id', currentUserId)
 
-        const response = await actions.addRack(formData)
-        if (response.status === 201 || response.status === 200) {
-          toast.success("Successfully Registered")
-          console.log("Registro exitoso")
-          console.log(data)
-          setTimeout(() => {
-            navigate("/consult")
-          }, 2000)
+      const response = await actions.addRack(formData)
+      if (response.status === 201 || response.status === 200) {
+        toast.success("Successfully Registered")
+        console.log("Registro exitoso")
+        console.log(data)
+        setTimeout(() => {
+          navigate("/consult")
+        }, 2000)
 
-        } else {
-          toast.error("Error Registrando Rack")
-          console.log("Error en el registro de Rack ", response.status)
-        }
-      } catch (error) {
-        console.log("newRack: ", error)
+      } else {
+        toast.error("Error Registrando Rack")
+        console.log("Error en el registro de Rack ", response.status)
       }
-    
+    } catch (error) {
+      console.log("newRack: ", error)
+    }
+
   };
 
   return (
@@ -134,9 +134,14 @@ function Rack() {
       <div id="rackForm" className="container">
         <BasicRack handleFieldChange={handleFieldChange} data={data} currentUser={currentUser} />
 
-        <button className="btn btn-primary m-2"
+        <button className="btn btn-primary m-2 agregar"
           onClick={handleAddRack}
-          type="button">Agregar
+          type="button">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 3H12H8C6.34315 3 5 4.34315 5 6V18C5 19.6569 6.34315 21 8 21H11M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V11.8125" stroke="#fffffff" stroke-width="2"></path>
+            <path d="M17 15V18M17 21V18M17 18H14M17 18H20" stroke="#fffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+          Agregar
         </button>
       </div>
     </>
