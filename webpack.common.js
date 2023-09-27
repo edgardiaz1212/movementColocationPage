54206191e09ce685711a279835f02c919363e9b7
Edgar Diaz <ed35376@gmail.com>
2023-09-27 19:56:44 -0400

    logo

--- webpack.common.js
+++ webpack.common.js
@@ -52,7 +52,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
+        favicon: 'cantv.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
