ac3aa7214479b2bdad1a2fd8cddc6ad90cd2ed6f
Edgar Diaz <ed35376@gmail.com>
2023-09-27 19:44:11 -0400

    buttons

--- src/front/js/component/Buttons.jsx
+++ src/front/js/component/Buttons.jsx
@@ -250,11 +250,11 @@ function Buttons({ editLink, id, type }) {
         if (data.service_frontal) {
             serviceFrontal.check(data.service_frontal);
         }
-        
+
         if (data.service_back) {
             serviceBack.check(data.service_back);
         }
-        
+
         if (data.service_lateral) {
             serviceSide.check(data.service_lateral);
         }
@@ -318,12 +318,14 @@ function Buttons({ editLink, id, type }) {
     return (
         <>
             <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                <Link to={editLink} className="btn btn-primary btn-sm " >
-                    Editar
+                <Link to={editLink} className="btn button_edit btn-sm " >
+                Editar 
+      <svg class="svg" viewBox="0 0 512 512">
+        <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg>
                 </Link>
                 <button className="btn btn-primary btn-sm buttonDownload"
                     onClick={handleFillPDF}>
-                        
+
                     Planilla
                 </button>
                 <button
